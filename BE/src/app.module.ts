efbda75c336ad6da6ea5d5d6f33ebd060c3a9314
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-10-05 19:42:00 +0600

    product resource created

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeormModule } from './db/typeorm.module';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UserModule } from './user/user.module';
       debug: true,
     }),
     UserModule,
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService],
